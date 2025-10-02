bc69de2f5b08e01a0536d9578f77728a407d0f4c
afedjah <458386@fontys.student.nl>
2025-10-02 12:54:11 +0200

    pom.xml changed

--- Trivia_backend/demo/src/main/java/com/trivia/demo/config/CorsConfig.java
+++ Trivia_backend/demo/src/main/java/com/trivia/demo/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.trivia.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${HOSTED_FRONTEND_URL}")
+    @Value("${HOSTED_FRONTEND_URL:http://localhost:4200}")
     private String hostedFrontendUrl;
 
     @Bean
@@ -17,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", hostedFrontendUrl)
+                        .allowedOrigins(hostedFrontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
