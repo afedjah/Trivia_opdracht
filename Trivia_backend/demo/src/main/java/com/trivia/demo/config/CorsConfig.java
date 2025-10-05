97f71e17e07742e9d910468da69e08be84791c80
afedjah <458386@fontys.student.nl>
2025-10-05 20:54:00 +0200

    cors update

--- Trivia_backend/demo/src/main/java/com/trivia/demo/config/CorsConfig.java
+++ Trivia_backend/demo/src/main/java/com/trivia/demo/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${HOSTED_FRONTEND_URL:http://localhost:4200}")
+    @Value("${HOSTED_FRONTEND_URL}")
     private String hostedFrontendUrl;
 
     @Bean
